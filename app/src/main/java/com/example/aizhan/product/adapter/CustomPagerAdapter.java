2e8f9ba1f5c43b8c9678b1c6ea049b4dd30651aa
ruslan <smarus93@gmail.com>
2017-06-27 16:36:59 +0600

    refactoring-3

--- app/src/main/java/com/example/aizhan/product/adapter/CustomPagerAdapter.java
+++ app/src/main/java/com/example/aizhan/product/adapter/CustomPagerAdapter.java
@@ -3,6 +3,7 @@ package com.example.aizhan.product.adapter;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,13 +13,14 @@ import com.bumptech.glide.Glide;
 import com.example.aizhan.product.ImageActivity;
 import com.example.aizhan.product.ModelObject;
 import com.example.aizhan.product.R;
+import com.github.chrisbanes.photoview.PhotoView;
 
 
 public class CustomPagerAdapter extends PagerAdapter {
     private Context context;
     private ModelObject dataObjectList;
     private LayoutInflater layoutInflater;
-    private ImageView displayImage;
+    private PhotoView displayImage;
     private Boolean mainPage;
     public CustomPagerAdapter(Context context, ModelObject dataObjectList,Boolean mainPage){
         this.context = context;
@@ -37,7 +39,7 @@ public class CustomPagerAdapter extends PagerAdapter {
     @Override
     public Object instantiateItem(ViewGroup container, final int position) {
         View view = this.layoutInflater.inflate(R.layout.item_view_pager, container, false);
-        displayImage = (ImageView)view.findViewById(R.id.image_for_jeens);
+        displayImage = (PhotoView) view.findViewById(R.id.photo_view);
         displayImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
